0cad310177836972b22829616f71186fc251886e
Debashis Debnath <frankendeba@Debashiss-MacBook-Air.local>
2023-07-16 14:56:48 +0530

    carousel fix

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import CarouselComp from "../Carousel";
+import "./Home.css";
 
 function Home() {
   return (
-    <div>
+    <div className="home__container">
       Home
       <CarouselComp />
     </div>
